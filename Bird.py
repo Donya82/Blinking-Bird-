fcf25fd5a2ba35c9de1e490b16f73a8d8da061a6
Melissa S <90488042+melissaamshun@users.noreply.github.com>
2023-04-30 21:25:07 -0700

    Update Bird.py

--- Bird.py
+++ Bird.py
@@ -4,6 +4,7 @@ import time
 from grove_rgb_lcd import *
 grovepi.set_bus("RPI_1") # set I2C to use the hardware bus
 
+
 ultrasonic_ranger = 4 # Connect the Grove Ultrasonic Ranger to digital port D4
 range = 150 #mid point of range
 birdpos = (posx, posy)
@@ -17,11 +18,11 @@ adc_ref = 5
 # Vcc of the grove interface is normally 5v
 grove_vcc = 5
 while cnt = 1:
-    #pipe 
-     pipeonepos = (rand1, 2)
-     pipeonepos = (rand2, 6)
-     posx = 10 #last pixel of lcd
-     cnt = 0 
+ #pipe 
+ pipeonepos = (rand1, 2)
+ pipeonepos = (rand2, 6)
+ posx = 10 #last pixel of lcd
+ cnt = 0 
 
  
 while cnt = 0:
@@ -41,16 +42,22 @@ while cnt = 0:
   posy = count +1
 
   if(posy == rand1 or rand2)
-    if(posx == 2 or 6):
-        #Red light 
-        #Reset score
-        #Send score 
-        #go to lose page 
-  elif(posx =0)
-    #Pause
-    #Score update (pop up screen) check if last point win or loss 
-    #Green light 
-    #next player
+   if(posx == 2 or 6):
+    #Red light 
+    led = 3
+    digitalWrite(led,1)
+    time.sleep(1)
+    #Reset score
+    #Send score 
+    #go to lose page 
+  elif(posx == 0)
+   #Pause
+   #Score update (pop up screen) check if last point win or loss 
+   #Green light 
+   led = 2
+   digitalWrite(led,1)
+   time.sleep(1)
+   #next player
 
     
  
