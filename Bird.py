81f01b1e1006df830e19cbfb2c0e649106b6c686
Donya82 <123035772+Donya82@users.noreply.github.com>
2023-05-04 14:12:11 -0700

    Update Bird.py

--- Bird.py
+++ Bird.py
@@ -19,54 +19,54 @@ adc_ref = 5
 # Vcc of the grove interface is normally 5v
 grove_vcc = 5
 cnt = 1
+while True:
+  while cnt == 1: #when cnt=1 game is in the begining set up phase 
+    #set player (1 or 2)
 
-while cnt == 1: #when cnt=1 game is in the begining set up phase 
-  #set player (1 or 2)
-  
-  #position and print the pipes
-  cursor_pos = (rand1, 4)
-  setText( "!")
-  cursor_pos = (rand2, 6)
-  setText( "!")
-  
-  #set the initial conditions of bird 
-  posx = 16 #last pixel of lcd
-  cnt = 0 #start the game 
-  
-while cnt == 0:#start the game
-    # moves flappy up and down
-   dist= ultrasonicRead(ultrasonic_ranger) # Read distance value from Ultrasonic
-   time.sleep(0.1) # don't overload the i2c bus 
-   sensor_value = 150 #set midway sensor value
-   if(dist >= sensor_value): #if distance is higher or equal than the threshhold flappy moves up
-    posy = 1
-   elif(dist < sensor_value):  #if distance is lower than the threshhold flappy moves down 
-    posy = 0    
-    
-   # moves flappy forward
-   posx = posx - 1 
- 
-   #print bird
-   cursor_pos = (1, 8) 
-   setText( "@")
-   time.sleep(0.5)
-    
-  #check if bird hit pipe or won
-   if(posy == rand1 or posy == rand2):
-    if(posx == 2 or posx ==6 ):
-     #Red light 
-     #Reset score
-     #Send score 
-     #go to lose page 
-     cnt=1
-   elif(posx == 0):
-    #Pause
-    #Score update (pop up screen) check if last point win or loss 
-    #Green light 
-    #next player
-    cnt =1
-   else:
-    cnt =0
-       
+    #position and print the pipes
+    cursor_pos = (rand1, 4)
+    setText( "!")
+    cursor_pos = (rand2, 6)
+    setText( "!")
+
+    #set the initial conditions of bird 
+    posx = 16 #last pixel of lcd
+    cnt = 0 #start the game 
+
+  while cnt == 0:#start the game
+      # moves flappy up and down
+     dist= ultrasonicRead(ultrasonic_ranger) # Read distance value from Ultrasonic
+     time.sleep(0.1) # don't overload the i2c bus 
+     sensor_value = 150 #set midway sensor value
+     if(dist >= sensor_value): #if distance is higher or equal than the threshhold flappy moves up
+      posy = 1
+     elif(dist < sensor_value):  #if distance is lower than the threshhold flappy moves down 
+      posy = 0    
+
+     # moves flappy forward
+     posx = posx - 1 
+
+     #print bird
+     cursor_pos = (1, 8) 
+     setText( "@")
+     time.sleep(0.5)
+
+    #check if bird hit pipe or won
+     if(posy == rand1 or posy == rand2):
+      if(posx == 2 or posx ==6 ):
+       #Red light 
+       #Reset score
+       #Send score 
+       #go to lose page 
+       cnt=1
+     elif(posx == 0):
+      #Pause
+      #Score update (pop up screen) check if last point win or loss 
+      #Green light 
+      #next player
+      cnt =1
+     else:
+      cnt =0
+
 
 
