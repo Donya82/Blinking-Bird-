3ac3d14796fa10235c472698c404eefb3dd1499c
Donya82 <123035772+Donya82@users.noreply.github.com>
2023-05-04 13:12:02 -0700

    Update Bird.py

--- Bird.py
+++ Bird.py
@@ -1,16 +1,16 @@
 
 from grovepi import *
 import time
-import random
+import
 from grove_rgb_lcd import *
-grovepi.set_bus("RPI_1") # set I2C to use the hardware bus
 
+grovepi.set_bus("RPI_1") # set I2C to use the hardware bus
 
 ultrasonic_ranger = 4 # Connect the Grove Ultrasonic Ranger to digital port D4
 range = 150 #mid point of range
 birdpos = (posx, posy)
-rand1 = random.randint(0, 1)
-rand2 = random.randint(0, 1)
+rand1 = random.randint(0,1)
+rand2 = random.randint(0,1)
 
 time.sleep(1)
 
@@ -18,12 +18,15 @@ time.sleep(1)
 adc_ref = 5
 # Vcc of the grove interface is normally 5v
 grove_vcc = 5
-while cnt = 1:
- #pipe 
- pipeonepos = (rand1, 2)
- pipeonepos = (rand2, 6)
- posx = 10 #last pixel of lcd
- cnt = 0 
+while cnt = 1: 
+    #pipe 
+     lcd.cursor_pos = (rand1, 2)
+     lcd.write_string( "!")
+     lcd.cursor_pos = (rand2, 6)
+     lcd.write_string( "!")
+
+     posx = 10 #last pixel of lcd
+     cnt = 0 
 
  
 while cnt = 0:
@@ -35,33 +38,38 @@ while cnt = 0:
   sensor_value = range
 
   if(dist >= sensor_value): #if distance is higher or equal than the threshhold flappy moves up
-   posx = 1
+   posy = 1
   
   elif(dist < sensor_value):  #if distance is lower than the threshhold flappy moves down 
-   posx = 0    
+   posy = 0    
   
-  posy = count +1
+  posx = posx - 1 # moves flappy forward
+
+  lcd.cursor_pos = (posy, posx) 
+  lcd.write_string( "@")
+  time.sleep(0.5)
+
+
+
 
   if(posy == rand1 or rand2)
-   if(posx == 2 or 6):
-    #Red light 
-    led = 3
-    digitalWrite(led,1)
-    time.sleep(1)
-    #Reset score
-    #Send score 
-    #go to lose page 
-  elif(posx == 0)
-   #Pause
-   #Score update (pop up screen) check if last point win or loss 
-   #Green light 
-   led = 2
-   digitalWrite(led,1)
-   time.sleep(1)
-   #next player
+    if(posx == 2 or 6):
+        #Red light 
+        #Reset score
+        #Send score 
+        #go to lose page 
+        cnt=1
+  elif(posx =0):
+    #Pause
+    #Score update (pop up screen) check if last point win or loss 
+    #Green light 
+    #next player
+    cnt =1
+  else :
 
     
  
  except IOError:
   
   print ("Error angle")
+
