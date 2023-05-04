08ae85e39603b3715e42c025e4ca04eb0c4b55b1
Donya82 <123035772+Donya82@users.noreply.github.com>
2023-05-04 13:45:39 -0700

    Update Bird.py

--- Bird.py
+++ Bird.py
@@ -18,55 +18,61 @@ time.sleep(1)
 adc_ref = 5
 # Vcc of the grove interface is normally 5v
 grove_vcc = 5
-while cnt == 1: 
-    #pipe 
-     lcd.cursor_pos = (rand1, 4)
-     lcd.write_string( "!")
-     lcd.cursor_pos = (rand2, 6)
-     lcd.write_string( "!")
-
-     posx = 16 #last pixel of lcd
-     cnt = 0 
-
- 
-while cnt == 0:
- 
- try:  
-  dist= grovepi.ultrasonicRead(ultrasonic_ranger) # Read distance value from Ultrasonic
-
-  time.sleep(0.1) # don't overload the i2c bus 
-  sensor_value = range
-
-  if(dist >= sensor_value): #if distance is higher or equal than the threshhold flappy moves up
-   posy = 1
-  
-  elif(dist < sensor_value):  #if distance is lower than the threshhold flappy moves down 
-   posy = 0    
-  
-  posx = posx - 1 # moves flappy forward
-
-  lcd.cursor_pos = (posy, posx) 
-  lcd.write_string( "@")
-  time.sleep(0.5)
-
-
-
-
-  if(posy == rand1 or posy == rand2):
-    if(posx == 2 or posx ==6 ):
-        #Red light 
-        #Reset score
-        #Send score 
-        #go to lose page 
-        cnt=1
-  elif(posx == 0):
-    #Pause
-    #Score update (pop up screen) check if last point win or loss 
-    #Green light 
-    #next player
-    cnt =1
+def main():
     
-  else:
-    cnt =0
+    while cnt == 1: 
+        #pipe 
+         lcd.cursor_pos = (rand1, 4)
+         lcd.write_string( "!")
+         lcd.cursor_pos = (rand2, 6)
+         lcd.write_string( "!")
 
+         posx = 16 #last pixel of lcd
+         cnt = 0 
+
+
+    while cnt == 0:
+
+     try:  
+      dist= grovepi.ultrasonicRead(ultrasonic_ranger) # Read distance value from Ultrasonic
+
+      time.sleep(0.1) # don't overload the i2c bus 
+      sensor_value = range
+
+      if(dist >= sensor_value): #if distance is higher or equal than the threshhold flappy moves up
+       posy = 1
+
+      elif(dist < sensor_value):  #if distance is lower than the threshhold flappy moves down 
+       posy = 0    
+
+      posx = posx - 1 # moves flappy forward
+
+      lcd.cursor_pos = (posy, posx) 
+      lcd.write_string( "@")
+      time.sleep(0.5)
+
+
+
+
+      if(posy == rand1 or posy == rand2):
+        if(posx == 2 or posx ==6 ):
+            #Red light 
+            #Reset score
+            #Send score 
+            #go to lose page 
+            cnt=1
+      elif(posx == 0):
+        #Pause
+        #Score update (pop up screen) check if last point win or loss 
+        #Green light 
+        #next player
+        cnt =1
+
+      else:
+        cnt =0
+
+        
+
+if __name__ == '__main__':
+    main()
 
