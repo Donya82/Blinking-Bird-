d4ecceb787046a65fb611a85e22fe5eedb8a983e
Donya82 <123035772+Donya82@users.noreply.github.com>
2023-05-04 14:04:10 -0700

    Update Bird.py

--- Bird.py
+++ Bird.py
@@ -8,8 +8,8 @@ grovepi.set_bus("RPI_1") # set I2C to use the hardware bus
 
 ultrasonic_ranger = 4 # Connect the Grove Ultrasonic Ranger to digital port D4
 #range = 150 #mid point of range
-birdpos = (posx, posy)
-rand1 = random.randint(0,1)
+
+rand1 = random.randint(0,1) #setting random ints for the position of the pipes
 rand2 = random.randint(0,1)
 
 time.sleep(1)
@@ -19,29 +19,39 @@ adc_ref = 5
 # Vcc of the grove interface is normally 5v
 grove_vcc = 5
 
-while cnt == 1: 
-  #pipe 
+
+while cnt == 1: #when cnt=1 game is in the begining set up phase 
+  #set player (1 or 2)
+  
+  #position and print the pipes
   lcd.cursor_pos = (rand1, 4)
   lcd.write_string( "!")
   lcd.cursor_pos = (rand2, 6)
   lcd.write_string( "!")
+  
+  #set the initial conditions of bird 
   posx = 16 #last pixel of lcd
-  cnt = 0 
-while cnt == 0:
-  try:  
+  cnt = 0 #start the game 
+  
+while cnt == 0:#start the game
+    # moves flappy up and down
    dist= grovepi.ultrasonicRead(ultrasonic_ranger) # Read distance value from Ultrasonic
    time.sleep(0.1) # don't overload the i2c bus 
-   sensor_value = 150
+   sensor_value = 150 #set midway sensor value
    if(dist >= sensor_value): #if distance is higher or equal than the threshhold flappy moves up
     posy = 1
    elif(dist < sensor_value):  #if distance is lower than the threshhold flappy moves down 
     posy = 0    
-
-   posx = posx - 1 # moves flappy forward
+    
+   # moves flappy forward
+   posx = posx - 1 
+ 
+   #print bird
    lcd.cursor_pos = (posy, posx) 
    lcd.write_string( "@")
    time.sleep(0.5)
-
+    
+  #check if bird hit pipe or won
    if(posy == rand1 or posy == rand2):
     if(posx == 2 or posx ==6 ):
      #Red light 
