bb548df891a0bc43d7915c8e5c3a4d8102360a03
Melissa S <90488042+melissaamshun@users.noreply.github.com>
2023-05-05 15:15:22 -0700

    we are gettint somewhere

--- Score.py
+++ Score.py
@@ -1,1 +1,47 @@
+from typing import Dict, List, Optional
+from flask import Flask, request, jsonify
+import pathlib
+import uuid
+import json
 
+app = flask.Flask(__name__)
+thisdir = pathlib.Path(__file__).parent.absolute()
+
+def load_leaderboard() -> Dict[str, str]]:
+  """
+    Loads the leaderboard
+   """
+  try:
+   return json.loads(thisdir.joinpath('blinkingScores.json').read_text())
+  except FileNotFoundError:
+   return []
+
+def save_leaderboard(lboard: Dict[str, str]]) -> None:
+    """
+      Saves the leaderboard
+    """
+    thisdir.joinpath('blinkingScores.json').write_text(json.dumps(lboard, indent=4))
+
+@app.route('/leaderboard/sendscore', methods=['POST'])
+def receive_new_score():
+  """
+   Appends leaderboard with new score
+  """
+  mes = request.get_data()
+  print(str(mes))
+  return str(mes)
+
+@app.route('/leaderboard/makeboard', methods=['GET'])
+def print_leaderboard():
+    """
+     Takes the top 5 scores from the leaderboard and prints them 
+    """
+    
+
+
+if __name__ == "__main__":
+  app.run()
+    
+    
+    
+    
