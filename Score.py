9076f14bed514aa0e108ffb78ba4e5a9982135e6
Melissa S <90488042+melissaamshun@users.noreply.github.com>
2023-05-05 23:20:24 -0700

    Update Score.py

--- Score.py
+++ Score.py
@@ -8,7 +8,7 @@ import json
 app = Flask(__name__)
 thisdir = pathlib.Path(__file__).parent.absolute()
 
-def load_leaderboard() -> List[Dict[str, str]]:
+def load_leaderboard() -> List[Dict[str, int]]:
   """
     Loads the leaderboard
    """
@@ -17,14 +17,14 @@ def load_leaderboard() -> List[Dict[str, str]]:
   except FileNotFoundError:
    return []
 
-def save_leaderboard(lboard: List[Dict[str, str]]):
+def save_leaderboard(lboard: List[Dict[str, int]]):
     """
       Saves the leaderboard
     """
     thisdir.joinpath('blinkingScores.json').write_text(json.dumps(lboard, indent=4))
 
 @app.route('/leaderboard/sendscore', methods=['POST', 'GET'])
-def receive_new_score() -> List[Dict[str, str]]:
+def receive_new_score() -> List[Dict[str, int]]:
   """
    Appends leaderboard with new score
   """
