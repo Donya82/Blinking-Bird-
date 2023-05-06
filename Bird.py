662a532f3271f6388c4c1baafcc31f2058811733
Melissa S <90488042+melissaamshun@users.noreply.github.com>
2023-05-05 23:19:50 -0700

    Update Bird.py

--- Bird.py
+++ Bird.py
@@ -12,7 +12,7 @@ IP = "172.20.10.7"
 #melissa: 172.20.10.7
 #donya: 10.26.254.4
 
-def postScore(hostname: str, score: Dict[str, str]) -> List[Dict[str, str]]:
+def postScore(hostname: str, score: Dict[str, int]) -> List[Dict[str, int]]:
     """Post the winning score on leaderboard
     
     Args:
@@ -132,13 +132,13 @@ def main():
               setText("Player 1 Wins") 
               name1 = input("Enter Winner's name: ")
               scoreDict = {name1:score1}
-              ldrbrd = postScore(IP, str(scoreDict))
+              ldrbrd = postScore(IP, scoreDict)
               print(ldrbrd)
             elif score1< score2:
               setText("Player 2 Wins")
               name1 = input("Enter Winner's name: ")
               scoreDict = {name1:score1}
-              ldrbrd = postScore(IP, str(scoreDict))
+              ldrbrd = postScore(IP, scoreDict)
               print(ldrbrd)
             elif score1 == score2:
               setText("Tie")   
