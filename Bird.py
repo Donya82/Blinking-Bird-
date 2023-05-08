51b219b577b823750968b7ef59150077f87eb9e6
Melissa S <90488042+melissaamshun@users.noreply.github.com>
2023-05-08 12:38:07 -0700

    Update Bird.py

--- Bird.py
+++ Bird.py
@@ -26,7 +26,7 @@ def postScore(hostname: str, score: Dict[str, str]) -> List[Dict[str, str]]:
     post_score = requests.post(f"http://{hostname}:5000/leaderboard/sendscore", data=score)
     #request_leaderboard = requests.get(f"http://{hostname}:5000/leaderboard/makeboard")
     myboard = post_score.json()
-    for item in sorted_list:
+    for item in myboard:
         print(item)
     
 
